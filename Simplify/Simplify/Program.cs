240dabfb7e7e69752618534ac53754de27a3aaad
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-02-13 00:15:10 +0530

    Inline config and mark as `static readonly`

--- Simplify/Simplify/Program.cs
+++ Simplify/Simplify/Program.cs
@@ -1,16 +1,16 @@
 ﻿namespace Simplify;
 static class MainProgram
 {
+  // Load preferences from config file as an immutable object
+  static readonly JsonConfig Config = Preferences.LoadConfig();
+
   public static void Main(string[] args)
   {
-    // Load preferences [creates an immutable object]
-    JsonConfig prefs = Preferences.LoadConfig();
-
     // Process input arguments
     bool includeFolders = false;
     bool makeChangesPermanent = false;
 
-    string libraryPath = prefs.LibraryPath;
+    string libraryPath = Config.LibraryPath;
     if (args.Any())
     {
       if (args[0].Replace('\\', '/').Contains('/'))
@@ -35,7 +35,7 @@ static class MainProgram
     // Rename folders
     if (runtimePreferences.RenameFolders)
     {
-      string[] folders = Scan.Folders(libraryPath, prefs);
+      string[] folders = Scan.Folders(libraryPath, Config);
       if (folders.Any() && Print.FolderConfirmation(folders, makeChangesPermanent))
       {
         for (int i = folders.Length - 1; i >= 0; i--)
@@ -43,17 +43,17 @@ static class MainProgram
           // WARNING: Reversed loop order because innermost folder must be renamed first
           // Simplifying the outermost folder first will break address of inner folders
           string fullPath = folders[i];
-          Rename.SimplifyFolder(in prefs, in runtimePreferences, fullPath, ref counter);
+          Rename.SimplifyFolder(in Config, in runtimePreferences, fullPath, ref counter);
         }
       }
       Console.WriteLine("\n\n");
     }
 
     // Rename files
-    var files = Scan.Files(libraryPath, in prefs);
+    var files = Scan.Files(libraryPath, in Config);
     if (files.Any() && Print.FilesConfirmation(files, makeChangesPermanent))
     {
-      foreach (string fullPath in files) { Rename.SimplifyFile(in prefs, in runtimePreferences, fullPath, ref counter); }
+      foreach (string fullPath in files) { Rename.SimplifyFile(in Config, in runtimePreferences, fullPath, ref counter); }
     }
 
     // Print results
