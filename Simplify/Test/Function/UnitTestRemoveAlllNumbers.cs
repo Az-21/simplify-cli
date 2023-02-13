7d58c824c18bd6a6d75400d1e93dc109d31b1834
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-02-13 14:52:49 +0530

    Add unit test for remove numbers function

---
+++ Simplify/Test/Function/UnitTestRemoveAlllNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace Test;
+
+[TestClass]
+public sealed class UnitTestRemoveAlllNumbers
+{
+  [TestMethod]
+  public void Test1()
+  {
+    // Expected I/O
+    string filename = "(123) [456] {789} 0";
+    const string expected = "(   ) [   ] {   }  ";
+
+    // Test with removeNumbers set to true
+    Simplify.Function.RemoveNumbers(ref filename, true);
+    Assert.AreEqual(expected, filename);
+  }
+
+  [TestMethod]
+  public void Test2()
+  {
+    // Expected I/O
+    string filename = "(123) [456] {789} 0";
+    const string expected = "(123) [456] {789} 0";
+
+    // Test with removeNumbers set to false
+    Simplify.Function.RemoveNumbers(ref filename, false);
+    Assert.AreEqual(expected, filename);
+  }
+}
