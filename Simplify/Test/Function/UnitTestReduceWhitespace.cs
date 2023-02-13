700ee2ebf8f04c515bb32b92b13c1e5f2e07ba9c
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-02-13 15:57:20 +0530

    Add tests for reduce whitespace function

---
+++ Simplify/Test/Function/UnitTestReduceWhitespace.cs
@@ -0,0 +1,29 @@
+﻿namespace Test;
+
+[TestClass]
+public sealed class UnitTestReduceWhitespace
+{
+  [TestMethod]
+  public void Test1()
+  {
+    // Expected I/O
+    string filename = "        |   |       | ";
+    const string expected = "| | |";
+
+    // Test
+    Simplify.Function.ReduceWhitespace(ref filename);
+    Assert.AreEqual(expected, filename);
+  }
+
+  [TestMethod]
+  public void Test2()
+  {
+    // Expected I/O
+    string filename = "[\t]   (\t)  [H  E  V  C]";
+    const string expected = "[ ] ( ) [H E V C]";
+
+    // Test
+    Simplify.Function.ReduceWhitespace(ref filename);
+    Assert.AreEqual(expected, filename);
+  }
+}
