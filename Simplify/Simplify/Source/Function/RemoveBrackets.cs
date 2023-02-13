560feb40c2ad345b7d3d55e22ab58b45fb09d06e
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-02-13 23:14:44 +0530

    Improve logic to handle nested brackets

--- Simplify/Simplify/Source/Function/RemoveBrackets.cs
+++ Simplify/Simplify/Source/Function/RemoveBrackets.cs
@@ -2,35 +2,53 @@
 
 namespace Simplify;
 
+// Remove parentheses and its contents
 public static partial class Function
 {
-  // Remove parentheses and its contents
-  [GeneratedRegex(@"\(.*?\)")]
+  // Match innermost () https://stackoverflow.com/a/49533163/17753137
+  [GeneratedRegex(@"\((?:[^()])*\)")]
   private static partial Regex CurvedBracketContainerRegex();
 
   public static void RemoveCurvedBrackets(ref string input, in bool removeCurvedBrackets)
   {
     if (!removeCurvedBrackets) { return; }
     input = CurvedBracketContainerRegex().Replace(input, Space);
-  }
 
-  // Remove square brackets and its contents
-  [GeneratedRegex(@"\[.*?\]")]
+    // Rematch recursively to go from innermost () to outermost (...()...)
+    if (CurvedBracketContainerRegex().Match(input).Success) { RemoveCurvedBrackets(ref input, removeCurvedBrackets); }
+  }
+}
+
+// Remove square brackets and its contents
+public static partial class Function
+{
+  // Match innermost [] https://stackoverflow.com/a/49533163/17753137
+  [GeneratedRegex(@"\[(?:[^\[\]])*\]")]
   private static partial Regex SquareBracketContainerRegex();
 
   public static void RemoveSquareBrackets(ref string input, in bool removeSquareBrackets)
   {
     if (!removeSquareBrackets) { return; }
     input = SquareBracketContainerRegex().Replace(input, Space);
-  }
 
-  // Remove curved brackets and its contents
-  [GeneratedRegex(@"\{.*?\}")]
+    // Rematch recursively to go from innermost [] to outermost [...[]...]
+    if (SquareBracketContainerRegex().Match(input).Success) { RemoveSquareBrackets(ref input, removeSquareBrackets); }
+  }
+}
+
+// Remove curved brackets and its contents
+public static partial class Function
+{
+  // Match innermost {} https://stackoverflow.com/a/49533163/17753137
+  [GeneratedRegex(@"\{(?:[^{}])*\}")]
   private static partial Regex CurlyBracketContainerRegex();
 
   public static void RemoveCurlyBrackets(ref string input, in bool removeCurlyBrackets)
   {
     if (!removeCurlyBrackets) { return; }
     input = CurlyBracketContainerRegex().Replace(input, Space);
+
+    // Rematch recursively to go from innermost {} to outermost {...{}...}
+    if (CurlyBracketContainerRegex().Match(input).Success) { RemoveCurlyBrackets(ref input, removeCurlyBrackets); }
   }
 }
