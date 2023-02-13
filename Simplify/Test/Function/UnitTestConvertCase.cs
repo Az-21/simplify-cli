a10e0fb30604e8594119026f8a1876b9012095e4
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-02-13 21:02:57 +0530

    Add tests for convert to lowercase function

---
+++ Simplify/Test/Function/UnitTestConvertCase.cs
@@ -0,0 +1,29 @@
+﻿namespace Test;
+
+[TestClass]
+public sealed class UnitTestConvertCase
+{
+  [TestMethod]
+  public void Test1()
+  {
+    // Expected I/O
+    string filename = "[reZero] (USA) de7ta 火";
+    const string expected = "[rezero] (usa) de7ta 火";
+
+    // Test
+    Simplify.Function.ConvertToLowercase(ref filename, true);
+    Assert.AreEqual(expected, filename);
+  }
+
+  [TestMethod]
+  public void Test2()
+  {
+    // Expected I/O
+    string filename = "[reZero] (USA) de7ta 火";
+    const string expected = "[reZero] (USA) de7ta 火";
+
+    // Test inactive state
+    Simplify.Function.ConvertToLowercase(ref filename, false);
+    Assert.AreEqual(expected, filename);
+  }
+}
