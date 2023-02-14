a489d2aef602517c4397b964ee91074b1df06a37
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-02-14 14:26:50 +0530

    Add test for optimize articles function

---
+++ Simplify/Test/Function/UnitTestOptimizeArticles.cs
@@ -0,0 +1,29 @@
+﻿namespace Test;
+
+[TestClass]
+public sealed class UnitTestOptimizeArticles
+{
+  [TestMethod]
+  public void Test01()
+  {
+    // Expected I/O
+    string filename = "a an the planet moon A An aN tHE thE The the IN";
+    const string expected = "a an the planet moon a an an the the the the in";
+
+    // Test active state of article optimizer
+    Simplify.Function.OptimizeArticles(ref filename, true);
+    Assert.AreEqual(expected, filename);
+  }
+
+  [TestMethod]
+  public void Test02()
+  {
+    // Expected I/O
+    string filename = "a an the planet moon A An aN tHE thE The the IN";
+    const string expected = "a an the planet moon A An aN tHE thE The the IN";
+
+    // Test inactive state of article optimizer
+    Simplify.Function.OptimizeArticles(ref filename, false);
+    Assert.AreEqual(expected, filename);
+  }
+}
