7a2caceceed6bbcc19422b41f1e33827f233f7ab
Az-21 <39803478+Az-21@users.noreply.github.com>
2023-02-14 13:50:13 +0530

    Add tests for convert to sentence case function

--- Simplify/Test/Function/UnitTestConvertCase.cs
+++ Simplify/Test/Function/UnitTestConvertCase.cs
@@ -4,26 +4,64 @@
 public sealed class UnitTestConvertCase
 {
   [TestMethod]
-  public void Test1()
+  public void Test01()
   {
     // Expected I/O
     string filename = "[reZero] (USA) de7ta 火";
     const string expected = "[rezero] (usa) de7ta 火";
 
-    // Test
+    // Test active state of destructive lowercase converter
     Simplify.Function.ConvertToLowercase(ref filename, true);
     Assert.AreEqual(expected, filename);
   }
 
   [TestMethod]
-  public void Test2()
+  public void Test02()
   {
     // Expected I/O
     string filename = "[reZero] (USA) de7ta 火";
     const string expected = "[reZero] (USA) de7ta 火";
 
-    // Test inactive state
+    // Test inactive state of destructive lowercase converter
     Simplify.Function.ConvertToLowercase(ref filename, false);
+    Assert.AreEqual(expected, filename);
+  }
+
+  [TestMethod]
+  public void Test03()
+  {
+    // Expected I/O
+    string filename = "a reZero in the USA aNd uK";
+    const string expected = "A Rezero In The Usa And Uk";
+
+    // Test sentence case **without** smart capitalization
+    Simplify.Function.ConvertToSentenceCase(ref filename, true, false);
+    Assert.AreEqual(expected, filename);
+  }
+
+  [TestMethod]
+  public void Test04()
+  {
+    // Expected I/O
+    string filename = "a reZero in the USA aNd uK";
+    const string expected = "A reZero In The USA aNd uK";
+
+    // Test sentence case **with** smart capitalization
+    Simplify.Function.ConvertToSentenceCase(ref filename, true, true);
+    Assert.AreEqual(expected, filename);
+  }
+
+  [TestMethod]
+  public void Test05()
+  {
+    // Expected I/O
+    string filename = "a reZero in the USA aNd uK";
+    const string expected = "a reZero in the USA aNd uK";
+
+    // Test inactive state of sentence case | Activity of smartCapitalization should not affect output
+    Simplify.Function.ConvertToSentenceCase(ref filename, false, true);
+    Assert.AreEqual(expected, filename);
+    Simplify.Function.ConvertToSentenceCase(ref filename, false, false);
     Assert.AreEqual(expected, filename);
   }
 }
